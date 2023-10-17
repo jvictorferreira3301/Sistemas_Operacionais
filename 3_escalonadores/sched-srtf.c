4e4063202cc3dfc4dc1a79d3f18723206f4cf6c8
JoeMiran <joemiran06@gmail.com>
2023-10-17 15:51:51 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    No ramo Joel
    Your branch is up to date with 'origin/Joel'.
    
    Mudanças a serem submetidas:
    	renamed:    3_escalonadores/FIFO -> 3_escalonadores/FCFS
    	modified:   3_escalonadores/SJF
    	new file:   3_escalonadores/SRT
    	renamed:    3_escalonadores/sjf.c -> 3_escalonadores/sched-sjf.c
    	new file:   3_escalonadores/sched-srtf.c

---
+++ 3_escalonadores/sched-srtf.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+A função a seguir tem a finalidade de calcular o tempo de espera para cada processo em 
+uma lista de processos. O tempo de espera é calculado somando-se o tempo de execução do
+processo anterior com o tempo de espera do processo anterior.
+*/
+int waitingtime(int proc[], int n, int burst_time[], int wait_time[]) {
+    wait_time[0] = 0;                        // Tempo de espera para o primeiro processo é 0
+    for (int i = 1; i < n ; i++ )            
+    wait_time[i] = burst_time[i-1] + wait_time[i-1] ; //Calculo do tempo de espera
+    return 0;
+}
+
+
+/*
+Essa função calcula o tempo de retorno (turnaround time) para cada processo e o armazena no array tat[i]. 
+O tempo de retorno é a soma do tempo de execução e do tempo de espera do processo.
+*/
+int turnaroundtime(int proc[], int n, int burst_time[], int wait_time[], int tat[]){
+    int i;     // Tempo de retorno para o primeiro processo é o tempo de execução                              
+    for ( i = 0; i < n ; i++)               
+    tat[i] = burst_time[i] + wait_time[i]; //Calculo do tempo de retorno
+    return 0;
+}
+
+
+/*
+A função abaixo é responsável por calcular o tempo médio de espera e o tempo médio de retorno para um conjunto de processos. 
+Ela também imprime na saída padrão os tempos de espera e os tempos de retorno de cada processo. 
+*/
+int avgtime( int proc[], int n, int burst_time[]) {
+
+    int wait_time[n], tat[n], total_wt = 0, total_tat = 0;
+    int i; 
+
+    waitingtime(proc, n, burst_time, wait_time);             
+    turnaroundtime(proc, n, burst_time, wait_time, tat);  
+    printf("Processo  Execução     Espera   Retorno \n");  
+
+    for ( i=0; i<n; i++) {                                   
+        total_wt = total_wt + wait_time[i];                   
+        total_tat = total_tat + tat[i];
+        printf(" %d\t  %d\t\t %d \t%d\n", i+1, burst_time[i], wait_time[i], tat[i]);
+    }
+
+    printf("Tempo médio de espera = %.2f\n", (float)total_wt / (float)n);
+    printf("Tempo médio de retorno = %.2f\n", (float)total_tat / (float)n);
+    return 0;
+}
+
+/*
+A função abaixo é responsável por implementar o escalonador SRT (Shortest Remaining Time).
+Ela recebe os mesmos parâmetros que a função avgtime e ordena os processos de acordo com o tempo restante de execução.
+Em seguida, executa o processo com o menor tempo restante primeiro.
+*/
+int srt(int proc[], int n, int burst_time[]) {
+
+    int wait_time[n], tat[n], total_wt = 0, total_tat = 0;
+    int remaining_time[n];                //Tempo de execução restante de cada processo
+    int smallest, time = 0, finish_time; //smallest é o processo com o menor tempo restante de execução
+    int count = 0;                      //Contador de processos executados
+    float avg_wt, avg_tat;
+
+    /*
+    Inicializando o tempo de execucao restante com o tempo inicial de cada processo
+    */
+    for (int i = 0; i < n; i++) {
+        remaining_time[i] = burst_time[i]; 
+    }
+
+    while (count != n) {
+        smallest = n + 1;
+        for (int i = 0; i < n; i++) {
+            if (remaining_time[i] > 0 && remaining_time[i] < remaining_time[smallest]) {
+                smallest = i;
+            }
+        }
+        remaining_time[smallest]--;
+        if (remaining_time[smallest] == 0) {
+            count++;
+            finish_time = time + 1;
+            wait_time[smallest] = finish_time - burst_time[smallest];
+            if (wait_time[smallest] < 0) {
+                wait_time[smallest] = 0;
+            }
+            tat[smallest] = burst_time[smallest] + wait_time[smallest];
+            total_wt += wait_time[smallest];
+            total_tat += tat[smallest];
+        }
+        time++;
+    }
+
+    printf("Processo  Execução     Espera   Retorno \n");  
+
+    for ( int i = 0; i < n; i++) {                                   
+        printf(" %d\t  %d\t\t %d \t%d\n", i+1, burst_time[i], wait_time[i], tat[i]);
+    }
+
+    avg_wt = (float)total_wt / (float)n;
+    avg_tat = (float)total_tat / (float)n;
+    printf("Tempo médio de espera = %.2f\n", avg_wt);
+    printf("Tempo médio de retorno = %.2f\n", avg_tat);
+    return 0;
+}
+
+
+int main() {
+
+    int n = 30;
+    int proc[n];
+    int burst_time[n];
+
+    srand(time(NULL)); //Gerador de números aleatórios
+
+    for (int i = 0; i < n; i++) {
+         proc[i] = i+1;
+         int random_time = rand() % 3; // gera um número aleatório entre 0 e 2
+         if (random_time == 0) {
+              burst_time[i] = 5;
+         } else if (random_time == 1) {
+              burst_time[i] = 8;
+         } else {
+              burst_time[i] = 12;
+         }
+    }
+
+    srt(proc, n, burst_time); //Chamada da função que implementa o escalonador SRT e calcula o tempo medio de retorno
+    return 0;
+}
