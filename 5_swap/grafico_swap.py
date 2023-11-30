bf1ec2818803f91d53a017d185e19a329b9178ee
Marco Maues <marcomauesjr@gmail.com>
2023-11-30 13:08:40 -0300

    Geração dos graficos

--- 5_swap/grafico_swap.py
+++ 5_swap/grafico_swap.py
@@ -2,56 +2,86 @@ import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
-arquivos_swap = glob.glob('*gb.txt')
-memoria = {}
-for arquivo in arquivos_swap:
-    df = pd.read_table(arquivo, sep='\s+', skiprows=1 )
-    nome_arquivo_sem_extensao = os.path.splitext(os.path.basename(arquivo))[0]
-    memoria[nome_arquivo_sem_extensao] = df
+def identify_axes(ax_dict, fontsize=48):
+    """
+    Helper to identify the Axes in the examples below.
 
+    Draws the label in a large font in the center of the Axes.
 
-# for dicionario in memoria:
-#     plt.title(f'Gráfico de {dicionario}')
-#     (-memoria[dicionario]['livre'] + (7.62*1024*1024)).plot()
-#     plt.autoscale()
-#     plt.tight_layout()
-#     plt.show()
-
-num_plots = len(memoria)
-fig, axs = plt.subplots(1, num_plots, figsize=(15, 5))
-
-for i, dicionario in enumerate(memoria):
-    axs[i].set_title(f'Gráfico de {dicionario}')
-    axs[i].plot(-memoria[dicionario]['livre'] + (7.62 * 1024 * 1024))
-    axs[i].autoscale()
-    axs[i].set_xlabel('Tempo (s)')
-    axs[i].set_ylabel('Memória Usada')
-    axs[i].grid(True)
-plt.tight_layout()
-plt.show()
+    Parameters
+    ----------
+    ax_dict : dict[str, Axes]
+        Mapping between the title / label and the Axes.
+    fontsize : int, optional
+        How big the label should be.
+    """
+    kw = dict(ha="center", va="center", fontsize=fontsize, color="darkgrey")
+    for k, ax in ax_dict.items():
+        ax.text(0.5, 0.5, k, transform=ax.transAxes, **kw)
 
 
-num_plots = len(memoria)
-fig, ax = plt.subplots(figsize=(15, 5))
+def plot_map(memoria):
+    num_plots = len(memoria)
+    # fig, axs = plt.subplots(1, num_plots, figsize=(15, 15))
 
-for i, dicionario in enumerate(memoria):
-    ax.plot(-memoria[dicionario]['livre'] + (7.62 * 1024 * 1024), label=f'{8*i}GB de memória de Swap')
+    # for i, dicionario in enumerate(memoria):
+        # axs[i].set_title(f'Gráfico de {8*i}GB de memória de Swap')
+        # axs[i].plot(-memoria[dicionario]['livre'] + (7.62 * 1024 * 1024))
+        # axs[i].autoscale()
+        # axs[i].set_xlabel('Tempo (s)')
+        # axs[i].set_ylabel('Quantidade de Memória usada (GB)')
+        # axs[i].grid(True)
+    # plt.tight_layout()
+    # plt.show()
 
-ax.set_title('Gráficos do Uso da Memória')
-ax.set_xlabel('Tempo (s)')
-ax.set_ylabel('Quantidade de Memória (GB)')
-ax.legend()
-ax.autoscale()
-ax.grid(True)
+    fig, axs = plt.subplots(2, num_plots, figsize=(15, 15))
 
-plt.tight_layout()
-plt.show()
+    for i, dicionario in enumerate(memoria):
+        axs[0,i].set_title(f'Gráfico de {8*i}GB de memória de Swap')
+        axs[0,i].plot(-memoria[dicionario]['livre'] + (7.62 * 1024 * 1024)) # O vmstat diz quanto de memoria livre temos, e quero verificar a memoria usada
+        axs[0,i].autoscale()
+        axs[0,i].set_xlabel('Tempo (s)')
+        axs[0,i].set_ylabel('Quantidade de Memória usada (GB)')
+        axs[0,i].grid(True)
+        axs[1,i].set_title(f'Gráfico do Uso da Memória de Swap')
+        axs[1,i].plot(memoria[dicionario]['swpd'])
+        axs[1,i].autoscale()
+        axs[1,i].set_xlabel('Tempo (s)')
+        axs[1,i].set_ylabel('Quantidade de Memória usada (GB)')
+        axs[1,i].grid(True)
+    plt.tight_layout()
+    plt.show()
 
-    # if 'livre' in df.columns:
-    #     df['livre'].plot()
-    #     plt.title(f'Gráfico de {arquivo}')
-    #     plt.autoscale()
-    #     plt.show()
-    # else:
-    #     print(f'O arquivo {arquivo} não possui a coluna "livre"')
+
+
+
+    # fig, ax = plt.subplots(figsize=(15, 5))
+
+    # for i, dicionario in enumerate(memoria):
+        # ax.plot(-memoria[dicionario]['livre'] + (7.62 * 1024 * 1024), label=f'{8*i}GB de memória de Swap')
+
+    # ax.set_title('Gráficos do Uso da Memória')
+    # ax.set_xlabel('Tempo (s)')
+    # ax.set_ylabel('Quantidade de Memória (GB)')
+    # ax.legend()
+    # ax.autoscale()
+    # ax.grid(True)
+
+    # plt.tight_layout()
+    # plt.show()
+
+
+def main():
+    arquivos_swap = glob.glob('*gb.txt')
+    memoria = {}
+    for arquivo in arquivos_swap:
+        df = pd.read_table(arquivo, sep='\s+', skiprows=1 )
+        nome_arquivo_sem_extensao = os.path.splitext(os.path.basename(arquivo))[0]
+        memoria[nome_arquivo_sem_extensao] = df
+    plot_map(memoria)
+
+if __name__ == "__main__":
+    main()
+
