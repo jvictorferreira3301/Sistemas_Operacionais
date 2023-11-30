86deb06b2d1c3fd089494b61d13dbb02df397b74
Marco Maues <marcomauesjr@gmail.com>
2023-11-30 12:01:10 -0300

    script para teste das memorias de swap

---
+++ 5_swap/swap.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+######################################## Primeiro teste para sem swap: ########################################
+echo -e '\tTeste sem memória de Swap\n'
+
+# Desativar swap:
+#sudo swapoff /swapfile
+vmstat 1 60 > swap0gb.txt &
+PID=$!
+
+sleep 10 
+python3 memoria_swap.py
+wait $PID
+echo -e '\tTeste sem memória de Swap: Finalizado\n'
+
+######################################## Inicio do Segundo Teste com 8gb de swap ################################
+sleep 10
+echo -e '\tTeste com 8gb de swap\n'
+
+# Criação da memoria de swap
+
+echo 'Criação da memoria de swap de 8Gb'
+sudo dd if=/dev/zero of=/swapfile bs=1G count=8 &
+PID=$!
+wait $PID
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+vmstat 1 60 > swap8gb.txt &
+PID=$!
+
+sleep 10 
+python3 memoria_swap.py
+wait $PID
+echo -e '\n\tTeste com 8GB de Swap: Finalizado\n'
+
+######################################## Inicio do Terceiro Teste com 16gb de swap ##########################
+sleep 10
+sudo swapoff /swapfile
+sudo rm /swapfile
+
+echo -e '\tTeste com 16gb de swap\n'
+
+# Criação da memoria de swap
+
+echo 'Criação da memoria de swap de 16Gb'
+sudo dd if=/dev/zero of=/swapfile bs=1G count=16 &
+PID=$!
+wait $PID
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+vmstat 1 60 > swap16gb.txt &
+PID=$!
+sleep 10
+
+python3 memoria_swap.py
+wait $PID
+echo -e '\n\tTeste com 16gb de memória de Swap: Finalizada\n'
+sudo chmod 662 *gb.txt
+
+
